3ef8e86415a82ce0a16262113a427562d42753c3
Nate Seilaff <seilaffn@gmail.com>
2022-08-26 13:42:50 -0600

    First Commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+var newList = new List<string>();
+
+newList.Add("Assassins Creed");
+newList.Add("StarCraft");
+newList.Add("Warcraft");
+newList.Add("Gears Of War");
+newList.Add("Halo");
+newList.Add("Halo Reach");
+
+
+newList.OrderBy(x => x.Length).ToList().ForEach(Console.WriteLine);
